af5eaa6a157875823fe4fe83d29a27d06517ee40
kau14266 <133906495+kau14266@users.noreply.github.com>
2023-05-25 14:19:26 -0400

    Update Student2.java

--- student2/src/student2/Student2.java
+++ student2/src/student2/Student2.java
@@ -9,7 +9,7 @@ package student2;
  * @author ra200
  */
 public class Student2 {
-     private String FEET;
+     private String TOE;
     /**
      * @param args the command line arguments
      */
