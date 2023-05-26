4813324e220433baf0ce830786d0da2e61dd13c5
kau14266 <133906495+kau14266@users.noreply.github.com>
2023-05-25 20:13:10 -0400

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -16,7 +16,7 @@ public class CardTrick {
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
-        
+        Card luckyCard = new Card();
         for (int i=0; i<magicHand.length; i++)
         {
             Card cd = new Card();
